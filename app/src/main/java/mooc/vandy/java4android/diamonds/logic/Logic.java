ce8758f7612e85ac52f2435bcd5d9682fea68e0f
el_revelador <bsse1011@iit.du.ac.bd>
2020-09-13 12:53:23 +0600

    Commented for better understanding

--- app/src/main/java/mooc/vandy/java4android/diamonds/logic/Logic.java
+++ app/src/main/java/mooc/vandy/java4android/diamonds/logic/Logic.java
@@ -46,23 +46,46 @@ public class Logic
     public void process(int size) {
 
         // TODO -- add your code here
+
+        /*For better understanding, I used an outer loop and an inner loop. In this way I created a
+          2-dimensional structure for the diamond*/
+
+        /*Variables:
+        *   a. size: indicates the size of the diamond
+        *   b. height: indicates the height/ number of rows
+        *   c. width: indicates the width/ number of columns
+        * */
+
+        //This outer loop denotes the number of rows or height of the diamond.
         for(int height=0; height<2*size+1; height++){
+
+            //This inner loop denotes the number of columns or width of the diamond.
             for(int width=0; width<2*size+2; width++){
+
+                //This conditional checks in which (rows, columns) to put '+'
                 if((height==0 || height==2*size)&&(width==0 || width==2*size+1)) {
                     if(width==2*size+1) mOut.println('+');
                     else mOut.print('+');
                 }
+
+                //This conditional checks in which (rows, columns) to put '-'
                 else if(height==0||height==2*size) mOut.print('-');
+
+                //This conditional checks in which (rows, columns) to put '|'
                 else if(width==0||width==2*size+1) {
                     if(width==2*size+1) mOut.println('|');
                     else mOut.print('|');
                 }
+
+                //This conditional check is done in the middle row
                 else if(height==(2*size+1)/2){
                     if(width==1) mOut.print('<');
                     else if(width==2*size) mOut.print('>');
                     else if(size%2==0) mOut.print('-');
                     else if(size%2!=0) mOut.print('=');
                 }
+
+                //This conditional check is done on rows situated before the middle row
                 else if(height<(2*size+1)/2){
                     if(width == size+1-height) mOut.print('/');
                     else if(width>size+1-height && width < size+height){
@@ -72,6 +95,8 @@ public class Logic
                     else if(width == size+height) mOut.print('\\');
                     else mOut.print(' ');
                 }
+
+                //This conditional check is done on rows situated after the middle row
                 else if(height>(2*size+1)/2){
                     if(width == (height+1) - size) mOut.print('\\');
                     else if(width>(height+1) - size && width < 3*size-height){
